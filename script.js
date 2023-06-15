e7ea848940b936958a5a0148027ca3e5385f3916
ShivBhanKushWaha <shivbhankushwaha862003672001@gmail.com>
2023-06-15 21:51:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","MERN DEVELOPER","FULLSTACK DEVELOPER","WEB DESIGNER","GRAPHIC DESIGNER","PROBLEM SOLVER DSA "],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+});
